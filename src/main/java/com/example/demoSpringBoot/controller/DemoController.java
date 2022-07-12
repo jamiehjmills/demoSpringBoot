609db4a71cd9668c13ddfe4131a8eb7fab17f79a
Hyojoo Mills <hyojoo.mills@gmail.com>
2022-07-12 21:17:43 +1000

    fixed some issues

--- src/main/java/com/example/demoSpringBoot/controller/DemoController.java
+++ src/main/java/com/example/demoSpringBoot/controller/DemoController.java
@@ -1,10 +1,9 @@
-package controller;
+package com.example.demoSpringBoot.controller;
 
 import com.example.demoSpringBoot.FirstName;
-import entity.Employee;
-import entity.EmployeeRepo;
+import com.example.demoSpringBoot.entity.Employee;
+import com.example.demoSpringBoot.entity.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,19 +17,18 @@ public class DemoController {
 
     Employee employees;
 
-    @Autowired
     EmployeeRepo db;
 
     int id = 0;
 
     @GetMapping("/hello")
-    @ResponseBody
+    //@ResponseBody
     public void response(@RequestParam String lastName){
 
         id++;
 
-        //this.employees = new Employee(id, firstName.getFirstName(), "test", lastName);
-       // db.save(employees);
+        this.employees = new Employee(id, firstName.getFirstName(), "test", lastName);
+        db.save(employees);
 
         System.out.println(id +" "+ firstName.getFirstName() + " " + lastName);
     }
--- src/main/java/com/example/demoSpringBoot/entity/Employee.java
+++ src/main/java/com/example/demoSpringBoot/entity/Employee.java
@@ -1,4 +1,4 @@
-package entity;
+package com.example.demoSpringBoot.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
