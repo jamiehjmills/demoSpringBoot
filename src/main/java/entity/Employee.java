d6f3141f7c29e9f3fff085b18d4e58e3a413092d
Hyojoo Mills <hyojoo.mills@gmail.com>
2022-07-12 20:23:56 +1000

    updated application properties

---
+++ src/main/java/entity/Employee.java
@@ -0,0 +1,29 @@
+package entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long customer_id;
+    private String first_name;
+    private String middle_name;
+    private String last_name;
+
+}
+
